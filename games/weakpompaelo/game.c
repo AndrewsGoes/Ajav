5f17d398eb16689d07a33f8ad7f74d0079ca16c7
AndrewsGoes <andrews.goes.2004@gmail.com>
2026-01-17 01:45:13 -0300

    add: weakpompaelo | change: gitignore revert games and resources/games

---
+++ games/weakpompaelo/game.c
@@ -0,0 +1,48 @@
+#include <raylib.h>
+#include <emscripten/emscripten.h>
+
+static Rectangle player = {400, 400, 40, 40};
+static Camera2D camera = {0};
+
+static const int screenWidth = 1280;
+static const int screenHeight = 720;
+
+static void UpdateDrawFrame(void);
+
+int main(){
+	InitWindow(screenWidth, screenHeight, "weakpompaelo");
+
+	camera.target = (Vector2){player.x, player.y};
+	camera.offset = (Vector2){50, screenHeight/2};
+
+	emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
+
+	CloseWindow();
+	return 0;
+}
+
+static void UpdateDrawFrame(void){
+		ClearBackground(DARKGRAY);
+		if(IsKeyDown(KEY_DOWN)){
+			player.y += 2;
+		}
+		if(IsKeyDown(KEY_UP)){
+			player.y -= 2;
+		}
+		if(IsKeyDown(KEY_RIGHT)){
+			player.x += 2;
+		}
+		if(IsKeyDown(KEY_LEFT)){
+			player.x -= 2;
+		}
+
+		camera.target = (Vector2){player.x, player.y};
+		camera.zoom = 1.0f;
+		BeginDrawing();
+			BeginMode2D(camera);
+				DrawRectangle(400, 400, 50, 50, WHITE);
+				DrawRectangleRec(player, RED);
+			EndMode2D();
+			DrawText("hello, world", 0, 0, 10, WHITE);
+		EndDrawing();
+}
--- main.c
+++ main.c
@@ -21,9 +21,11 @@ Color guiRect = {0xff, 0xff, 0xff, 0xff};
 
 // Main menu images
 Texture2D menuLogo;
+Texture2D games[1];
 
 // Main menu variables
 Vector2 menuLogoPosition;
+Vector2 gamesPosition[1];
 Rectangle recGames[MAX_GAMES] = { 0 };
 Rectangle recGamesCoord = {45.0f, 160.0f, 100.0f, 100.0f};
 Vector2 mousePos;
@@ -34,15 +36,18 @@ static const int screenHeight = 720;
 static void UpdateDrawFrame(void);
 
 EM_JS(void, redirect_page, (), {
-	window.location.href = "https://www.google.com";		
+	window.location.href = "games/weakpompaelo/index.html";		
 })
 
 int main(void){
     InitWindow(screenWidth, screenHeight, "Ajav - Raylib Friv");
 
     menuLogo = LoadTexture("resources/main_menu/ajav_logo_NOBG.png");
+    games[0] = LoadTexture("resources/games/weakpompaelo.png");
     menuLogoPosition.x = screenWidth/2 - menuLogo.width/2;
     menuLogoPosition.y = screenHeight/2 - menuLogo.height/2 - 300;
+    gamesPosition[0].x = recGamesCoord.x;
+    gamesPosition[0].y = recGamesCoord.y;
 
     for(int i = 1; i < MAX_GAMES; i++){
 	recGames[i].x = recGamesCoord.x;
@@ -65,11 +70,12 @@ int main(void){
 static void UpdateDrawFrame(void){
 	ClearBackground(menuBackground);
 	mousePos = GetMousePosition();
-	if(IsKeyDown(KEY_SPACE)){
-		redirect_page();
-	}
 	BeginDrawing();
+		if(CheckCollisionPointRec(mousePos, recGames[1]) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+			redirect_page();
+		}
 		DrawTextureV(menuLogo, menuLogoPosition, WHITE);
+		DrawTextureV(games[0], gamesPosition[0], WHITE);
 		DrawLine(0, 120, 1280, 120, DARKGRAY);
 		DrawRectangleLines(25, 140, screenWidth - 50, screenHeight - 165, guiRect);
 		for(int i = 0; i < MAX_GAMES; i++){
