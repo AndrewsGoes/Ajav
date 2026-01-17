4c7c9a66bb35909fdc5654e0165f2cd55afc8d27
AndrewsGoes <andrews.goes.2004@gmail.com>
2026-01-17 10:50:54 -0300

    add: add player text

--- games/weakpompaelo/game.c
+++ games/weakpompaelo/game.c
@@ -30,10 +30,10 @@ static void UpdateDrawFrame(void){
 			player.y -= 2;
 		}
 		if(IsKeyDown(KEY_RIGHT)){
-			player.x -= 2;
+			player.x += 2;
 		}
 		if(IsKeyDown(KEY_LEFT)){
-			player.x += 2;
+			player.x -= 2;
 		}
 
 		camera.target = (Vector2){player.x, player.y};
@@ -42,7 +42,9 @@ static void UpdateDrawFrame(void){
 			BeginMode2D(camera);
 				DrawRectangle(400, 400, 50, 50, WHITE);
 				DrawRectangleRec(player, RED);
+				DrawText("player", player.x + 6, player.y + 12, 10, BLUE);
 			EndMode2D();
+			DrawText("player", player.x - 20.0f, player.y - 20.0f, 10, DARKGRAY);
 			DrawText("jogo teste, nao esta completo ainda!", 0, 0, 10, WHITE);
 		EndDrawing();
 }
