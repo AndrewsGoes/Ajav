4987bf0e4e06667a8449cb907700acc92ebc2093
AndrewsGoes <andrews.goes.2004@gmail.com>
2026-01-20 11:48:25 -0300

    weakpompaelo add: pause when exit the window, new sprite and some mechanics

--- games/weakpompaelo/game.c
+++ games/weakpompaelo/game.c
@@ -1,19 +1,36 @@
 #include <raylib.h>
+#include <raymath.h>
+#include <stdio.h>
 #include <emscripten/emscripten.h>
 
-static Rectangle player = {400, 400, 40, 40};
+static Rectangle Scene1 = {0, 130, 1280, 125};
+static Rectangle Scene2 = {1280, 130, 1280, 125};
+static float loopReference = 1280.0f;
+static Vector2 player = {175, 100};
 static Camera2D camera = {0};
+
+static Texture2D playerSprite;
+
+static bool pause = false;
 
 static const int screenWidth = 1280;
 static const int screenHeight = 720;
 
 static void UpdateDrawFrame(void);
 
+EM_JS(bool, exitWindow, (), {
+	return document.hidden || document.visiblityState === 'hidden';
+})
+
 int main(){
 	InitWindow(screenWidth, screenHeight, "weakpompaelo");
+	
+	playerSprite = LoadTexture("resources/player.png");
 
-	camera.target = (Vector2){player.x, player.y};
-	camera.offset = (Vector2){50, screenHeight/2};
+	camera.target = (Vector2){0, -screenHeight/2};
+	camera.offset = (Vector2){0, 0};
+	camera.rotation = 0.0f;
+	camera.zoom = 1.0f;
 
 	emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
 
@@ -22,28 +39,47 @@ int main(){
 }
 
 static void UpdateDrawFrame(void){
-		ClearBackground(DARKGRAY);
-		if(IsKeyDown(KEY_DOWN)){
-			player.y += 2;
-		}
-		if(IsKeyDown(KEY_UP)){
-			player.y -= 2;
-		}
-		if(IsKeyDown(KEY_RIGHT)){
-			player.x += 2;
-		}
-		if(IsKeyDown(KEY_LEFT)){
-			player.x -= 2;
+		float deltaTime = GetFrameTime();
+		if(exitWindow() == true){
+			pause = true;
 		}
 
-		camera.target = (Vector2){player.x, player.y};
-		camera.zoom = 1.0f;
+		if(IsKeyPressed(KEY_P) && pause){
+			pause = false;
+		}
+
+		if(!pause){
+			if(Scene1.x > -loopReference){
+				Scene1.x -= 501 * deltaTime;
+			}else{
+				Scene1.x += loopReference*2;
+			}
+
+			if(Scene2.x > -loopReference){
+				Scene2.x -= 501 * deltaTime;
+			}else{
+				Scene2.x += loopReference*2;
+			}
+		
+			if(IsKeyDown(KEY_LEFT)){
+				player.x -= 100 * deltaTime;
+			}
+			if(IsKeyDown(KEY_RIGHT)){
+				camera.target.x += 50 * deltaTime;
+				player.x += 100 * deltaTime;
+			}
+		}
+		ClearBackground(DARKGRAY);
 		BeginDrawing();
 			BeginMode2D(camera);
-				DrawRectangle(400, 400, 50, 50, WHITE);
-				DrawRectangleRec(player, RED);
-				DrawText("player", player.x + 6, player.y + 12, 10, BLUE);
+				DrawRectangleV((Vector2){Scene1.x, Scene1.y + player.y},(Vector2){Scene1.width, Scene1.height}, WHITE);
+				DrawRectangleV((Vector2){Scene2.x, Scene2.y + player.y},(Vector2){Scene2.width, Scene2.height}, BLUE);
+				DrawTextureV(playerSprite, player, WHITE);
 			EndMode2D();
-			DrawText("jogo teste, nao esta completo ainda!", 0, 0, 50, WHITE);
+			if(!pause){
+				DrawText("em producao...", 0, 0, 50, WHITE);
+			}else{
+				DrawText("pausado!", 0, 0, 50, RED);
+			}
 		EndDrawing();
 }
